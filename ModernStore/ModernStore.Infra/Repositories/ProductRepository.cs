0eb8399cf16e521532325e130106ab9fc365ed37
Wescley Matos <wescleymatos@gmail.com>
2018-06-24 21:37:44 -0300

    Implementação dos repositórios e do pattern Unity of Work

---
+++ ModernStore/ModernStore.Infra/Repositories/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using ModernStore.Domain.Entities;
+using ModernStore.Domain.Repositories;
+using ModernStore.Infra.Contexts;
+using System;
+using System.Linq;
+
+namespace ModernStore.Infra.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ModernStoreDataContext _context;
+
+        public ProductRepository(ModernStoreDataContext context)
+        {
+            _context = context;
+        }
+
+        public Product Get(Guid id)
+        {
+            return _context.Products.AsNoTracking().FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ ModernStore/ModernStore.Infra/Transactions/Uow.cs
@@ -0,0 +1,22 @@
+﻿using ModernStore.Infra.Contexts;
+using System;
+
+namespace ModernStore.Infra.Transactions
+{
+    public class Uow : IUow
+    {
+        private readonly ModernStoreDataContext _context;
+
+        public Uow(ModernStoreDataContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Rollback() { }
+    }
+}
