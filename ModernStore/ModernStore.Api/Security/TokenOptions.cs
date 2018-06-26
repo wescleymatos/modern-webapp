26e4d128bb1214578e1565496702691a819df692
Wescley Matos <wescleymatos@gmail.com>
2018-06-26 19:01:30 -0300

    Configuração do JWT

---
+++ ModernStore/ModernStore.Api/Security/TokenOptions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Threading.Tasks;
+
+namespace ModernStore.Api.Security
+{
+    public class TokenOptions
+    {
+        public string Issuer { get; set; }
+        public string Subject { get; set; }
+        public string Audience { get; set; }
+        public DateTime NotBefore { get; set; } = DateTime.Now;
+        public DateTime IssuedAt { get; set; } = DateTime.Now;
+        public TimeSpan ValidFor { get; set; } = TimeSpan.FromDays(30);
+        public DateTime Expiration => IssuedAt.Add(ValidFor);
+
+        public Func<Task<string>> JtiGenerator => () => Task.FromResult(Guid.NewGuid().ToString());
+        public SigningCredentials SigningCregentials { get; set; }
+    }
+}
--- ModernStore/ModernStore.Api/Startup.cs
+++ ModernStore/ModernStore.Api/Startup.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using ModernStore.Domain.Commands.Handlers;
 using ModernStore.Domain.Repositories;
 using ModernStore.Domain.Services;
@@ -8,14 +11,28 @@ using ModernStore.Infra.Contexts;
 using ModernStore.Infra.Repositories;
 using ModernStore.Infra.Services;
 using ModernStore.Infra.Transactions;
+using System.Text;
 
 namespace ModernStore.Api
 {
     public class Startup
     {
+        private const string ISSUER = "33g3543g3rh";
+        private const string AUDIENCE = "3dfvger78revew";
+        private const string SECRET_KEY = "e223d6e6-c17f-4a1e-baa9-4f28fd8f6ebc";
+
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SECRET_KEY));
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
             services.AddCors();
 
             services.AddScoped<ModernStoreDataContext, ModernStoreDataContext>();
@@ -33,7 +50,7 @@ namespace ModernStore.Api
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
 
-            app.UseCors(x => 
+            app.UseCors(x =>
             {
                 x.AllowAnyHeader();
                 x.AllowAnyMethod();
